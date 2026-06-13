d497a81e04f36f827cfc839ae5cc8517f10a6d1d
protap1100 <protapb23@gmail.com>
2026-06-13 23:19:54 +0600

    all data added

--- query.sql
+++ query.sql
@@ -38,8 +38,8 @@ CREATE TABLE
         booking_id INT PRIMARY KEY,
         user_id INT NOT NULL,
         match_id INT NOT NULL,
-        seat_number VARCHAR(10) NOT NULL,
-        payment_status VARCHAR(20) NOT NULL CHECK (
+        seat_number VARCHAR(10),
+        payment_status VARCHAR(20) CHECK (
             payment_status IN ('Pending', 'Confirmed', 'Cancelled', 'Refunded')
         ),
         total_cost DECIMAL(10, 2) CHECK (total_cost >= 0),
@@ -47,3 +47,34 @@ CREATE TABLE
         FOREIGN KEY (match_id) REFERENCES Matches (match_id),
         UNIQUE (match_id, seat_number)
     );
+
+
+-- Inserting Data
+
+-- Users
+INSERT INTO Users (user_id, full_name, email, role, phone_number)
+VALUES
+(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
+(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+
+-- Matches
+INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status)
+VALUES
+(101, 'Real Madrid vs Barcelona', 'Champions League', 150, 'Available'),
+(102, 'Man City vs Liverpool', 'Premier League', 120, 'Selling Fast'),
+(103, 'Bayern Munich vs PSG', 'Champions League', 130, 'Available'),
+(104, 'AC Milan vs Inter Milan', 'Serie A', 90, 'Sold Out'),
+(105, 'Juventus vs Roma', 'Serie A', 80, 'Available');
+
+-- Bookings
+INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost)
+VALUES
+(501, 1, 101, 'A-12', 'Confirmed', 150),
+(502, 1, 102, 'B-04', 'Confirmed', 120),
+(503, 2, 101, 'A-13', 'Confirmed', 150),
+(504, 2, 101, NULL, NULL, 150),
+(505, 3, 102, 'C-20', 'Pending', 120);
+
+
