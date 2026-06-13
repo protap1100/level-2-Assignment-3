d3e6bb50d789e3e911ce9dc589749ebe42d0a087
protap1100 <protapb23@gmail.com>
2026-06-14 00:12:04 +0600

    all query added

--- query.sql
+++ query.sql
@@ -48,55 +48,124 @@ CREATE TABLE
         UNIQUE (match_id, seat_number)
     );
 
-
 -- Inserting Data
-
 -- Users
-INSERT INTO Users (user_id, full_name, email, role, phone_number)
+INSERT INTO
+    Users (user_id, full_name, email, role, phone_number)
 VALUES
-(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
-(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
-(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
-(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+    (
+        1,
+        'Tanvir Rahman',
+        'tanvir@mail.com',
+        'Football Fan',
+        '+8801711111111'
+    ),
+    (
+        2,
+        'Asif Haque',
+        'asif@mail.com',
+        'Football Fan',
+        '+8801722222222'
+    ),
+    (
+        3,
+        'Sajjad Rahman',
+        'sajjad@mail.com',
+        'Ticket Manager',
+        '+8801733333333'
+    ),
+    (
+        4,
+        'Jannat Ara',
+        'jannat@mail.com',
+        'Football Fan',
+        NULL
+    );
 
 -- Matches
-INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status)
+INSERT INTO
+    Matches (
+        match_id,
+        fixture,
+        tournament_category,
+        base_ticket_price,
+        match_status
+    )
 VALUES
-(101, 'Real Madrid vs Barcelona', 'Champions League', 150, 'Available'),
-(102, 'Man City vs Liverpool', 'Premier League', 120, 'Selling Fast'),
-(103, 'Bayern Munich vs PSG', 'Champions League', 130, 'Available'),
-(104, 'AC Milan vs Inter Milan', 'Serie A', 90, 'Sold Out'),
-(105, 'Juventus vs Roma', 'Serie A', 80, 'Available');
+    (
+        101,
+        'Real Madrid vs Barcelona',
+        'Champions League',
+        150,
+        'Available'
+    ),
+    (
+        102,
+        'Man City vs Liverpool',
+        'Premier League',
+        120,
+        'Selling Fast'
+    ),
+    (
+        103,
+        'Bayern Munich vs PSG',
+        'Champions League',
+        130,
+        'Available'
+    ),
+    (
+        104,
+        'AC Milan vs Inter Milan',
+        'Serie A',
+        90,
+        'Sold Out'
+    ),
+    (
+        105,
+        'Juventus vs Roma',
+        'Serie A',
+        80,
+        'Available'
+    );
 
 -- Bookings
-INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost)
+INSERT INTO
+    Bookings (
+        booking_id,
+        user_id,
+        match_id,
+        seat_number,
+        payment_status,
+        total_cost
+    )
 VALUES
-(501, 1, 101, 'A-12', 'Confirmed', 150),
-(502, 1, 102, 'B-04', 'Confirmed', 120),
-(503, 2, 101, 'A-13', 'Confirmed', 150),
-(504, 2, 101, NULL, NULL, 150),
-(505, 3, 102, 'C-20', 'Pending', 120);
-
-
-
+    (501, 1, 101, 'A-12', 'Confirmed', 150),
+    (502, 1, 102, 'B-04', 'Confirmed', 120),
+    (503, 2, 101, 'A-13', 'Confirmed', 150),
+    (504, 2, 101, NULL, NULL, 150),
+    (505, 3, 102, 'C-20', 'Pending', 120);
 
 -- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
-SELECT 
+SELECT
     match_id,
     fixture,
     base_ticket_price
-FROM Matches
-WHERE tournament_category = 'Champions League'
-AND match_status = 'Available';
+FROM
+    Matches
+WHERE
+    tournament_category = 'Champions League'
+    AND match_status = 'Available';
 
 -- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
 SELECT
     user_id,
     full_name,
     email
-FROM Users
-WHERE full_name ILIKE 'Tanvir%'
-   OR full_name ILIKE '%Haque%';
+FROM
+    Users
+WHERE
+    full_name ILIKE 'Tanvir%'
+    OR full_name ILIKE '%Haque%';
 
 -- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
 SELECT
@@ -104,15 +173,59 @@ SELECT
     user_id,
     match_id,
     COALESCE(payment_status, 'Action Required') AS systematic_status
-FROM Bookings
-WHERE payment_status IS NULL;
+FROM
+    Bookings
+WHERE
+    payment_status IS NULL;
 
 -- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+SELECT
+    b.booking_id,
+    u.full_name,
+    m.fixture,
+    b.total_cost
+FROM
+    Bookings b
+    INNER JOIN Users u ON b.user_id = u.user_id
+    INNER JOIN Matches m ON b.match_id = m.match_id;
 
 -- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
-
+SELECT
+    u.user_id,
+    u.full_name,
+    b.booking_id
+FROM
+    Users u
+    LEFT JOIN Bookings b ON u.user_id = b.user_id
+ORDER BY
+    u.user_id,
+    b.booking_id;
 
 -- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
-
+SELECT
+    booking_id,
+    match_id,
+    total_cost
+FROM
+    Bookings
+WHERE
+    total_cost > (
+        SELECT
+            AVG(total_cost)
+        FROM
+            Bookings
+    );
 
 -- Query 7: Retrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match.
+SELECT
+    match_id,
+    fixture,
+    base_ticket_price
+FROM
+    Matches
+ORDER BY
+    base_ticket_price DESC
+LIMIT
+    2
+OFFSET
+    1;
