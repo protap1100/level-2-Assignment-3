6475fd2acf8ce6ecbf6a8ab2e3601e89661d31e0
protap1100 <protapb23@gmail.com>
2026-06-15 21:00:30 +0600

    final push

--- query.sql
+++ query.sql
@@ -17,7 +17,7 @@ CREATE TABLE
     Matches (
         match_id INT PRIMARY KEY,
         fixture VARCHAR(100) NOT NULL,
-        tournament_category VARCHAR(50) NOT NULL,
+        tournament_category VARCHAR(100) NOT NULL,
         base_ticket_price DECIMAL(10, 2) NOT NULL CHECK (base_ticket_price >= 0),
         match_status VARCHAR(30) NOT NULL CHECK (
             match_status IN (
